71ec8d0ade88f7731d1401311edf2728975154ba
Thiagobw <thgleopoldo900@gmail.com>
2022-08-17 00:22:51 -0300

    first commit

---
+++ view/aprendendo bootstrap.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	 
+		<div class="row top">
+			<div class="col-12 col-sm-8 cabecalho1">pt1</div>
+			<div class="col-12 col-sm-4 cabecalho2">
+				<div class="row">
+					<div class="col-12 col-sm-4">t1</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row banner">
+			<div class="col-12 col-sm-12 imgbanner">
+			</div>
+		</div>
+
+		<nav>
+			<div class="row">
+				<div class="col-12 col-sm-12 menu"></div>
+			</div>
+		</nav>
+
+		<div class="row">
+			<div class="col-11 col-sm-6 col-md-2 col-lg-2 c c1">p1</div>
+			<div class="col-11 col-sm-6 col-md-3 col-lg-2 c c2">p2</div>
+			<div class="col-11 col-sm-6 col-md-3 col-lg-2 c c3">p3</div>
+			<div class="col-11 col-sm-6 col-md-3 col-lg-2 c c4">p4</div>
+			<div class="col-11 col-sm-6 col-md-3 col-lg-2 c c5">p5</div>
+			<div class="col-11 col-sm-6 col-md-3 col-lg-2 c c6">p6</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12 col-sm-5 p p1"></div>
+			<div class="col-12 col-sm-7 p p2"></div>
+		</div>
+
+		<footer class="rodape">
+			<div class="col-12 col-sm-12"></div>
+		</footer>
+
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ view/aprendendo bootstrap2.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style2.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 col-sm-12 topo">
+				<p>menu top</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12 col-sm-12 menu">
+				<p>menu</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12 col-sm-12 banner">
+					<img src="img/banner thempresa.png" alt="" class="img-fluid img-banner">
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row cx">
+				<div class="col-10 col-sm-5 col-md-3 col-lg-2 cxs">
+					<img src="img/erp.png" alt="" class="img-fluid img-produtos">
+					<h6 class="text-center title">ERP</h6>
+					<p class="text-center">Gestao empresarial, mais produtividade e controle dos custos.</p>
+				</div>
+
+				<div class="col-10 col-sm-5 col-md-3 col-lg-2 cxs">
+					<img src="img/rh.png" alt="" class="img-fluid img-produtos">
+					<h6 class="text-center title">RH</h6>
+					<p class="text-center">A gestão do seu RH muito mais assertiva, segura e inteligente.</p>
+				</div>
+
+				<div class="col-10 col-sm-5 col-md-3 col-lg-2 cxs">
+					<img src="img/saude.png" alt="" class="img-fluid img-produtos">
+					<h6 class="text-center title">Saude</h6>
+					<p class="text-center">Gestão integrada de todos os processos clinicos e administrativos.</p>
+				</div>
+
+				<div class="col-10 col-sm-5 col-md-3 col-lg-2 cxs">
+					<img src="img/juridico.png" alt="" class="img-fluid img-produtos">
+					<h6 class="text-center title">Juridico</h6>
+					<p class="text-center">Otimize os processos, gestão do seu departamento juridico.</p>
+				</div>
+
+				<div class="col-10 col-sm-5 col-md-3 col-lg-2 cxs">
+					<img src="img/logistica.png" alt="" class="img-fluid img-produtos">
+					<h6 class="text-center title">Logistica</h6>
+					<p class="text-center">Sua empresa mais ágil e competitiva, otimize processos e reduza seus custos.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-12 insigner">
+					<div class="row">
+					<div class="col-6 sobre">
+						<h2 class="text-center mt-2">quem somos!</h2>
+						<p class="mt-2">
+							Nós do Th sistemas somos uma instituição focada em automatização, desenvolvimo e créscimento da sua organização com foco na saúde organizacional da sua empresa.
+							 Nós do Th sistemas somos uma instituição focada em automatização, desenvolvimo e créscimento da sua organização com foco na saúde organizacional da sua empresa.
+							Nós do Th sistemas somos uma instituição focada em automatização, desenvolvimo e créscimento da sua organização com foco na saúde organizacional da sua empresa.
+						</p>
+					</div>
+
+					<div class="col-6 conta">
+						<h2 class="text-center mt-1">Receba novidades!</h2>
+						<p class="text-center">cadastre-se para comunicar-se conosco e receber orientações para o crescimento do seu negocio</p>
+						<form class="input-group center" action="">
+							<div class="col-8 mt-2">
+								<input class="form-control" type="text" placeholder="Nome completo">
+							</div>
+
+							<div class="col-8 mt-2">
+								<input class="form-control" type="text" placeholder="Email">
+							</div>
+
+							<div class="col-8 mt-2">
+								<input class="form-control" type="text" placeholder="Endereço">
+							</div>
+
+							<div class="col-8 mt-2">
+								<input class="form-control" type="text" placeholder="CPF">
+							</div>
+
+							<div class="col-6 mt-2 mb-3">
+								<input class="form-control btn btn-primary" type="submit" value="Enviar">
+							</div>
+						</form>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12 col-sm-12 rodape"></div>
+		</div>
+
+	</div>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
